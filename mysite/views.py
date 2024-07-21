df24282f9641d117c87663553003c594b812a18b
shenzhentdl <3676537674@qq.com>
2024-07-21 13:34:59 +0800

    修改index视图

--- mysite/views.py
+++ mysite/views.py
@@ -3,8 +3,8 @@ from datetime import datetime
 from django.shortcuts import render
 
 def index(request, tvno=0):
-    tv_list = [{'name': 'CCTV中文国际', 'tvcode': '9sE12tg3CmA?si=oLlwPhqmOr4BS7ni'},
-               {'name': '凤凰卫视资讯台', 'tvcode': 'bYBjjtgKUnM?si=SgGUL_PHTk8Pe0BA'}]
+    tv_list = [{'name': 'CCTV中文国际', 'tvcode': '/cctv4/?spm=C28340.PALq7Iyxwdh3.ExidtyEJcS5K.4'},
+               {'name': 'CCTV少儿频道', 'tvcode': 'cctvchild/?spm=C28340.PUdOXeMUrKth.ExidtyEJcS5K.26'}]
     now = datetime.now()
     tv = tv_list[tvno]
     return render(request, 'index.html', locals())
